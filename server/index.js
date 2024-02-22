3f7058d8c57176e8cdbce6c6e7f35faceb42440b
zaryab ikram <zaryabsheikh0@gmail.com>
2024-02-22 02:24:38 -0800

    hopwd

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use(express.static(path.join(__dirname, "/build")));
-//app.use(express.static(path.resolve(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.resolve(__dirname, "build")));
 //Multer File Storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
