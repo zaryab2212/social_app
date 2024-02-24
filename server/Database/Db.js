34f143fbfa204e5347f2a33ac94cc3c8b7d1bce6
zaryab ikram <zaryabsheikh0@gmail.com>
2024-02-24 16:02:31 +0500

    jfdkd

--- server/Database/Db.js
+++ server/Database/Db.js
@@ -5,13 +5,16 @@ exports.connection = async () => {
     // "mongodb+srv://jessiep1232:RuCaCHz0o1llwHAV@blogapp.ysghny1.mongodb.net/?retryWrites=true&w=majority"
 
     // mongodb+srv://jessiep1232:RuCaCHz0o1llwHAV@socialapp.e8jm7ik.mongodb.net/Social_App?retryWrites=true&w=majority
-    // "mongodb+srv://jessiep1232:jO6dUQpXbMGCHtog@socialapp.e8jm7ik.mongodb.net/?retryWrites=true&w=majority"
+    ("mongodb+srv://jessiep1232:jO6dUQpXbMGCHtog@socialapp.e8jm7ik.mongodb.net/?retryWrites=true&w=majority");
     //localhost:27017
-    await mongoose.connect("mongodb://localhost:27017/socialapp", {
-      useNewUrlParser: true,
+    await mongoose.connect(
+      "mongodb+srv://jessiep1232:jO6dUQpXbMGCHtog@socialapp.e8jm7ik.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
 
-      useUnifiedTopology: true,
-    });
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log("Data Base id connected succesfully");
   } catch (error) {
